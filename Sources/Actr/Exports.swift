2b83cdb256590f6f6ea2893ef6fdb0ad86af1e7c
Harry <128583280+kaito-harry@users.noreply.github.com>
2026-06-17 17:46:03 +0800

    feat(exports): add ErrorEvent typealias for ErrorEventBridge

--- Sources/Actr/Exports.swift
+++ Sources/Actr/Exports.swift
@@ -3,6 +3,7 @@ import ActrBindings
 /// Re-export commonly used types so applications can `import Actr`.
 public typealias Context = ContextBridge
 public typealias RpcEnvelope = RpcEnvelopeBridge
+public typealias ErrorEvent = ErrorEventBridge
 public typealias Workload = WorkloadLifecycleBridge
 public typealias DataStream = ActrBindings.DataStream
 public typealias DataStreamCallback = ActrBindings.DataStreamCallback
